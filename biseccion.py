f994a5fe48c5d8c5f74513cd4908344ee1c50b92
luntaboada <luntaboada@gmail.com>
2025-09-18 19:44:53 -0600

    biseccion (+ refinado)

--- biseccion.py
+++ biseccion.py
@@ -5,7 +5,7 @@ Metodos Numericos - Biseccion
 """
 
 def f(x):
-    return x - 100 
+    return x - 100 # Cambiar a cualquier ecuacion.
 
 def signo(a):
     if a == 0:
@@ -15,36 +15,49 @@ def signo(a):
     else: 
         return "+"
 
+
 i = int(0) 
 rango = [0, 0]
 rango[0] = float(input("limite inferior: ")) 
 rango[1] = float(input("limite superior: "))
 maxIter = int(input("numero maximo de iteraciones: "))
-print(f"0: {rango}")
 
+finf = f(rango[0])
+signoinf = signo(finf)
+
+fsup = f(rango[1])
+signosup = signo(fsup)
+
+print(f"0: {rango}")
 while i < maxIter:
     i = i+1
     nlim = (rango[0] + rango[1]) / 2
-    finf = f(rango[0])
-    fsup = f(rango[1])
     fnlim = f(nlim)
-    print("--------------------------------------------------")
-    print(f"nuevo limite: ({rango[0]} + {rango[1]}) / 2 = {nlim}")
-    print(f"f({rango[0]}) = {finf}")
-    print(f"f({rango[1]}) = {fsup}")
-    print(f"f({nlim}) = {fnlim}")
-    
+    nsigno = signo(fnlim)
 
-    if signo(fnlim) == signo(finf):
+    if nsigno == signoinf:
         rango[0] = nlim
-        print(f"{i}: {rango} : d={rango[1]-rango[0]}")
-
-    elif signo(fnlim) == signo(fsup):
+        finf = fnlim
+        signoinf = nsigno
+    
+    elif nsigno == signosup:
         rango[1] = nlim
-        print(f"{i}: {rango} : d={rango[1]-rango[0]}")
-
-    else:
-        rango = [nlim]
-        print(f"se encontro la raiz {rango}: f({rango}) = {fnlim}")
-        print("\n")
+        fsup = fnlim
+        signosup = nsigno
+    
+    else: 
+        rango = nlim
+        print(f"se encontro la raiz en {rango}")
         break
+
+    print(f"{i}: {rango}")
+
+
+"""
+A grandes rasgos, el algoritmo de biseccion consiste en: 
+1. Establecer un rango inicial [a, b] en el cual se sabe que existe una raiz.
+2. Calcular el punto medio del rango
+2.1. Si el punto medio tiene el mismo signo que el limite inferior, se sustituye el limite inferior por el punto medio.
+2.2. Si el punto medio tiene el mismo signo que el limite superior, se sustituye el limite superior por el punto medio.
+Cuando se llegue a un delta lo suficientemente pequeño, se puede considerar que el punto medio es la raiz. En este caso, el delta es el de maquina.
+"""
